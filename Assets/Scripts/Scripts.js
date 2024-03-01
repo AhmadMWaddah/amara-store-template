b8a9ceff41bf3fdc274fdc0e249a348d6b0dc0c0
AhmadMWaddah <wddh.ahmd@gmail.com>
2024-03-01 14:26:46 +0200

    Products JavaScripts
    Adding JavaScript for Products Section to Pick each Category and Showing Specific Items.

--- Assets/Scripts/Scripts.js
+++ Assets/Scripts/Scripts.js
@@ -1,27 +1,35 @@
 /* =--------------------------------------= Load Document =------------------= */
-window.addEventListener("load", function() {
+window.addEventListener("load", () => {
+
 
 	/* =--------------------------------------= Categories Swiper =------------------= */
 	var categoriesSwiper = new Swiper("#categories-wrapper", {
 		loop: true,
-		navigation: {
-			prevEl: ".swiper__button__next",
-			nextEl: ".swiper__button__prev",
-		},
+		navigation: { prevEl: ".swiper__button__next", nextEl: ".swiper__button__prev", },
 		breakpoints: {
-			640: {
-				slidesPerView: 2,
-				spaceBetween: 20,
-			},
-			768: {
-				slidesPerView: 4,
-				spaceBetween: 40,
-			},
-			1400: {
-				slidesPerView: 6,
-				spaceBetween: 24,
-			},
+			640: { slidesPerView: 2, spaceBetween: 20, },
+			768: { slidesPerView: 4, spaceBetween: 40, },
+			1400: { slidesPerView: 6, spaceBetween: 24, },
 		},
 	});
+
+
+	/* =--------------------------------------= Categories Swiper =------------------= */
+	const tabsButtons = document.querySelectorAll("[data-target]");
+	const tabsContents = document.querySelectorAll("[content]");
+	tabsButtons.forEach( (tabButton) => {
+		tabButton.addEventListener("click", () => {
+			const tabButtonTarget = document.querySelector(tabButton.dataset.target);
+			tabsContents.forEach( tabContent => {
+				tabContent.classList.remove("tab__item__active");
+			});
+			tabButtonTarget.classList.add("tab__item__active");
+			tabsButtons.forEach( tabButton => {
+				tabButton.classList.remove("tab__button__active");
+			});
+			tabButton.classList.add("tab__button__active");
+		});
+	});
+	
 
 });
