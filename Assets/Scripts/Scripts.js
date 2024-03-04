2563069a0c3e307bdc64c11b68945c20a392bace
AhmadMWaddah <wddh.ahmd@gmail.com>
2024-03-04 17:17:32 +0200

    Add Newsletter and Footer
    Adding NewsLetter and Footer Partition in home page - and moving html pages outside (Pages) Directory into Root Directory. and added Product Component.

--- Assets/Scripts/Scripts.js
+++ Assets/Scripts/Scripts.js
@@ -5,7 +5,7 @@ window.addEventListener("load", () => {
 	/* =--------------------------------------= Categories Swiper =------------------= */
 	var categoriesSwiper = new Swiper("#categories-wrapper", {
 		loop: true,
-		navigation: { prevEl: ".swiper__button__next", nextEl: ".swiper__button__prev", },
+		navigation: { prevEl: "#categories-swiper-button-prev", nextEl: "#categories-swiper-button-next", },
 		breakpoints: {
 			640: { slidesPerView: 2, spaceBetween: 20, },
 			768: { slidesPerView: 4, spaceBetween: 40, },
@@ -14,7 +14,19 @@ window.addEventListener("load", () => {
 	});
 
 
-	/* =--------------------------------------= Categories Swiper =------------------= */
+	/* =--------------------------------------= New Arrival Swiper =------------------= */
+	var newArrivalSwiper = new Swiper("#new-arrivals-wrapper", {
+		loop: true,
+		navigation: { prevEl: "#new-arrivals-swiper-button-prev", nextEl: "#new-arrivals-swiper-button-next", },
+		breakpoints: {
+			640: { slidesPerView: 2, spaceBetween: 20, },
+			768: { slidesPerView: 4, spaceBetween: 40, },
+			1400: { slidesPerView: 4, spaceBetween: 24, },
+		},
+	});
+
+
+	/* =--------------------------------------= Featured Products =------------------= */
 	const tabsButtons = document.querySelectorAll("[data-target]");
 	const tabsContents = document.querySelectorAll("[content]");
 	tabsButtons.forEach( (tabButton) => {
