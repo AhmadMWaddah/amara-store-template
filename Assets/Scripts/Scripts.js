de0775f12711cb7340ca49f9d8b69bc67b68dddc
AhmadMWaddah <wddh.ahmd@gmail.com>
2024-03-12 10:50:50 +0200

    Adding Responsive Menu - Responsive 1200px
    Adding Scripts and HTML and Sss for the Responsive Menu - added code html and SASS for responsive pages for the Media Query (max-width: 1200px).

--- Assets/Scripts/Scripts.js
+++ Assets/Scripts/Scripts.js
@@ -2,13 +2,29 @@
 window.addEventListener("load", () => {
 
 
+	/* =--------------------------------------= Toggle Mobile Menu =------------------= */
+	const responsiveNavBar = document.getElementById("navbar-menu");
+	const navBarToggle = document.getElementById("navbar-toggle");
+	const navBarClose = document.getElementById("navbar-close");
+	if (navBarToggle) {
+		navBarToggle.addEventListener("click", function() {
+			responsiveNavBar.classList.add("navbar__menu__opened");
+		});
+	};
+	if (navBarClose) {
+		navBarClose.addEventListener("click", function() {
+			responsiveNavBar.classList.remove("navbar__menu__opened");
+		});
+	};
+
+
 	/* =--------------------------------------= Categories Swiper =------------------= */
 	var categoriesSwiper = new Swiper("#categories-wrapper", {
 		loop: true,
 		navigation: { prevEl: "#categories-swiper-button-prev", nextEl: "#categories-swiper-button-next", },
 		breakpoints: {
-			640: { slidesPerView: 2, spaceBetween: 20, },
-			768: { slidesPerView: 4, spaceBetween: 40, },
+			768: { slidesPerView: 2, spaceBetween: 24, },
+			992: { slidesPerView: 3, spaceBetween: 24, },
 			1400: { slidesPerView: 6, spaceBetween: 24, },
 		},
 	});
@@ -19,9 +35,11 @@ window.addEventListener("load", () => {
 		loop: true,
 		navigation: { prevEl: "#new-arrivals-swiper-button-prev", nextEl: "#new-arrivals-swiper-button-next", },
 		breakpoints: {
-			640: { slidesPerView: 2, spaceBetween: 20, },
-			768: { slidesPerView: 4, spaceBetween: 40, },
-			1400: { slidesPerView: 4, spaceBetween: 24, },
+			350: { slidesPerView: 2, spaceBetween: 24, },
+			768: { slidesPerView: 3, spaceBetween: 24, },
+			992: { slidesPerView: 4, spaceBetween: 24, },
+			1200: { slidesPerView: 5, spaceBetween: 24, },
+			1400: { slidesPerView: 6, spaceBetween: 24, },
 		},
 	});
 
